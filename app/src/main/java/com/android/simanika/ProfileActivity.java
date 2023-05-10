9486e81cb582a95d14f8a3bb97f02b9b92d9a4c1
zakiaass <117088186+zakiaass@users.noreply.github.com>
2023-05-11 06:55:18 +0700

    Update Logout

--- app/src/main/java/com/android/simanika/ProfileActivity.java
+++ app/src/main/java/com/android/simanika/ProfileActivity.java
@@ -1,15 +1,18 @@
 package com.android.simanika;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.simanika.AuthFragment.LoginFragment;
+import com.android.simanika.Services.SharedPreference.Preferences;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -63,10 +66,33 @@ public class ProfileActivity extends AppCompatActivity {
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ProfileActivity.this, AuthActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                finish();
+                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+                builder.setTitle("Konfirmasi");
+                builder.setMessage("Apakah Anda yakin ingin logout?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Kode untuk logout
+                        // ...
+
+                        // Kembali ke halaman login
+                        Preferences.clearLoggedInUser(view.getContext());
+                        Activity thisActivity = ProfileActivity.this;
+                        thisActivity.finishAfterTransition();
+
+                        Intent intent = new Intent(thisActivity, AuthActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
 
